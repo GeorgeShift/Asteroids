9d11862e7d65c4e867a7db505dfa506e5e98c2d5
George Shift <koudy683@gmail.com>
2019-04-28 19:18:54 +0200

    Přidány asteroidy

--- asteroids.py
+++ asteroids.py
@@ -2,6 +2,9 @@ import pyglet
 from pyglet import gl
 from pyglet.window import key
 import math
+import random
+from random import randrange
+import os
 
 # Konstanty
 SIRKA = 800
@@ -9,22 +12,22 @@ VYSKA = 600
 RYCHLOST_OTACENI = 360
 AKCELERACE = 200
 
+# Soubory obrázků
+obrazek_lodi = 'ship.png'
+asteroidy_slozka = 'Meteors\\'
+asteroidy = []
+
 # Globální proměnné
 objekty = []  # globální seznam objektů
 stisknute_klavesy = set()  # sada stisknutych klaves
 batch = pyglet.graphics.Batch()  # kolekce s objekty pro vykreslení
 
 
-class Spaceship:
-    """ Třída reprezentující vesmírnou loď"""
-    def __init__(self, x, y, soubor):
-        # nastavení souradnic obrázku na střed hrací plochy
-        self.x = x
-        self.y = y
-        self.rychlost = 0
-        self.rotace = 0
+class VesmirnyObjekt:
+    """ Nadtřída reprezentující vesmírný objekt"""
+    def __init__(self, obrazek):
         # načtení obrázku ze souboru
-        obrazek = pyglet.image.load(soubor)
+        obrazek = pyglet.image.load(obrazek)
         # nastavní "kotvy" obrázku na střed
         obrazek.anchor_x = obrazek.width // 2
         obrazek.anchor_y = obrazek.height // 2
@@ -32,19 +35,7 @@ class Spaceship:
             obrazek, self.x,
             self.y, batch=batch)
 
-    # obsluha posouvání, natáčení a ovládání raketky
     def tick(self, dt):
-        if 'nahoru' in stisknute_klavesy:
-            self.rychlost += dt * AKCELERACE
-        if 'dolu' in stisknute_klavesy:
-            self.rychlost -= dt * AKCELERACE
-            if self.rychlost < 0:
-                self.rychlost = 0
-        if 'doleva' in stisknute_klavesy:
-            self.rotace -= RYCHLOST_OTACENI * dt
-        if 'doprava' in stisknute_klavesy:
-            self.rotace += RYCHLOST_OTACENI * dt
-
         rotace_rad = math.radians(-self.sprite.rotation + 90)
         self.x += self.rychlost * dt * math.cos(rotace_rad)
         self.y += self.rychlost * dt * math.sin(rotace_rad)
@@ -62,6 +53,46 @@ class Spaceship:
         self.sprite.rotation = self.rotace
 
 
+class Asteroid(VesmirnyObjekt):
+    """ Třída reprezentující asteroid"""
+    def __init__(self):
+        self.rychlost = randrange(0, 200)
+        self.rotace = randrange(0, 360)
+        if randrange(0, 2):
+            self.x = randrange(0, SIRKA)
+            self.y = 0
+        else:
+            self.x = 0
+            self.y = randrange(0, VYSKA)
+        # výběr náhodného asteroidu ze  seznamu
+        super().__init__(random.choice(asteroidy))
+
+
+class VesmirnaLod(VesmirnyObjekt):
+    """ Třída reprezentující vesmírnou loď"""
+    def __init__(self, obrazek=obrazek_lodi):
+        self.rychlost = 0
+        self.rotace = 0
+        # nastavení souradnic obrázku na střed hrací plochy
+        self.x = SIRKA // 2
+        self.y = VYSKA // 2
+        super().__init__(obrazek)
+
+    # obsluha posouvání, natáčení a ovládání raketky
+    def tick(self, dt):
+        if 'nahoru' in stisknute_klavesy:
+            self.rychlost += dt * AKCELERACE
+        if 'dolu' in stisknute_klavesy:
+            self.rychlost -= dt * AKCELERACE
+            if self.rychlost < 0:
+                self.rychlost = 0
+        if 'doleva' in stisknute_klavesy:
+            self.rotace -= RYCHLOST_OTACENI * dt
+        if 'doprava' in stisknute_klavesy:
+            self.rotace += RYCHLOST_OTACENI * dt
+        super().tick(dt)
+
+
 def vykresli_scenu():
     window.clear()
 
@@ -71,10 +102,8 @@ def vykresli_scenu():
             gl.glPushMatrix()
             # Move everything drawn from now on by (x_offset, y_offset, 0)
             gl.glTranslatef(x_offset, y_offset, 0)
-
             # Draw
             batch.draw()
-
             # Restore remembered state (this cancels the glTranslatef)
             gl.glPopMatrix()
 
@@ -101,10 +130,25 @@ def pusteni_klavesy(symbol, modifikatory):
         stisknute_klavesy.discard('doprava')
 
 
-lod = Spaceship(SIRKA // 2, VYSKA // 2, 'ship.png')
-objekty.append(lod)
+def obnov_stav(dt):
+    for objekt in objekty:
+        objekt.tick(dt)
 
-pyglet.clock.schedule_interval(objekty[0].tick, 1/100)
+
+# načtení všech asteroidů z adresáře do seznamu
+# r=root, d=directories, f = files
+for r, d, f in os.walk(asteroidy_slozka):
+    for file in f:
+        asteroidy.append(os.path.join(r, file))
+
+# Přidání lodi do hracího pole
+lod = VesmirnaLod()
+objekty.append(lod)
+# Přidání asteroidů do hracího pole
+for i in range(5):
+    objekty.append(Asteroid())
+
+pyglet.clock.schedule_interval(obnov_stav, 1/100)
 window = pyglet.window.Window(width=SIRKA, height=VYSKA)
 
 window.push_handlers(
